cafe4e7673a2f73e91dee135f3a580847734f6b4
code_vibe <mrsamoladapo@gmail.com>
2025-02-20 19:02:54 +0100

    Created a simple get route

--- hello_main/src/main.rs
+++ hello_main/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
+    axum::serve(listner,app).await.unwrap();
+
 }
+
+async  fn hello() -> &'static str { "Hello, World!.. What's up?" }
