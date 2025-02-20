8a23b022ba504faae9f9a19c0a5517c887e1a5ae
code_vibe <mrsamoladapo@gmail.com>
2025-02-20 19:23:50 +0100

    Serialized response

--- hello_main/src/main.rs
+++ hello_main/src/main.rs
@@ -1,9 +1,11 @@
-use axum::Router;
+use axum::http::StatusCode;
+use axum::{Json, Router};
 use axum::routing::get;
+use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/", get(hello_json));
 
     let listner = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
     axum::serve(listner,app).await.unwrap();
@@ -11,3 +13,14 @@ async fn main() {
 }
 
 async  fn hello() -> &'static str { "Hello, World!.. What's up?" }
+
+async fn hello_json() -> (StatusCode, Json<Response>){
+    let res = Response{
+        message: "Hello, World!.. What's up?",
+    };
+    (StatusCode::OK, Json(res))
+}
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
