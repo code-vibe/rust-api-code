56cba439ecba26ed3dcd5c3966c090a9d8544c88
code_vibe <mrsamoladapo@gmail.com>
2025-02-24 12:05:20 +0100

    Update serve file

--- cli_app/src/commands/mod.rs
+++ cli_app/src/commands/mod.rs
@@ -1,9 +1,12 @@
 mod hello;
+mod serve;
 
 use clap::{ArgMatches, Command};
 
 pub fn configure(command: Command) -> Command {
-    command.subcommand(hello::configure())
+    command
+        .subcommand(hello::configure())
+        .subcommand(serve::configure())
         .arg_required_else_help(true)
 }
 
@@ -11,6 +14,7 @@ pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
     if let Some((cmd, matches)) = matches.subcommand() {
         match cmd {
             hello::COMMAND_NAME => hello::handle(matches)?,
+            serve::COMMAND_NAME => serve::handle(matches)?,
             &_ => {}
         }
     }
