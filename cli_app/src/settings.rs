032b4c294c2dfc0698fa63b2f1717a48293fe061
code_vibe <mrsamoladapo@gmail.com>
2025-02-24 15:19:52 +0100

    Added Config file

---
+++ cli_app/src/settings.rs
@@ -0,0 +1,48 @@
+use std::panic::Location;
+use config::{Config, Environment, File};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize,Default)]
+#[allow(unused)]
+pub struct Database {
+    pub url: Option<String>,
+}
+#[derive(Debug, Deserialize, Default)]
+#[allow(unused)]
+pub struct ConfigInfo {
+    pub location: Option<String>,
+    pub env_prefix: Option<String>,
+}
+
+#[derive(Debug, Deserialize,Default)]
+#[allow(unused)]
+pub struct Logging {
+    pub log_level: Option<String>,
+}
+
+#[derive(Debug, Deserialize,Default)]
+#[allow(unused)]
+pub struct AppSettings {
+    #[serde(default)]
+    pub config: ConfigInfo,
+    #[serde(default)]
+    pub database: Database,
+    #[serde(default)]
+    pub logging: Logging,
+}
+
+impl AppSettings {
+    pub fn new(location: &str, env_prefix: &str) -> anyhow::Result<Self> {
+        let s = Config::builder()
+            .add_source(File::with_name(location))
+            .add_source(Environment::with_prefix(env_prefix)
+                .separator("_")
+                .prefix_separator("_"),
+            ).set_override("config.location", location)?
+            .set_override("config.env_prefix", env_prefix)?
+            .build()?;
+
+        let settings = s.try_deserialize()?;
+        Ok(settings)
+    }
+}
