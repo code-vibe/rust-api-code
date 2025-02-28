0c46cd68e3e5d45dfcb25e89525b886c31d958e6
code_vibe <mrsamoladapo@gmail.com>
2025-02-28 21:47:45 +0100

    Updated main

--- cli_app/src/main.rs
+++ cli_app/src/main.rs
@@ -1,14 +1,15 @@
 use clap::{Arg, Command};
+use dotenv::dotenv;
 use cli_app::{commands, settings};
 pub fn main() -> anyhow::Result<()> {
-
+    dotenv().ok();
     let mut command = Command::new("Sample CLI application")
         .arg(
             Arg::new("config")
                 .short('c')
                 .long("config")
-                .help("Configuration file location")
-                .default_value("config.json"),
+                .help("Configuration file location"),
+               // .default_value("config.json"),
         );
     command = commands::configure(command);
 
@@ -16,8 +17,8 @@ pub fn main() -> anyhow::Result<()> {
     let matches = command.get_matches();
     let config_location = matches
         .get_one("config")
-        .map(|s: &String| s.as_str())
-        .unwrap_or("");
+        .map(|s: &String| Some(s.as_str()))
+        .unwrap_or(None);
 
     commands::handle(&matches)?;
 
