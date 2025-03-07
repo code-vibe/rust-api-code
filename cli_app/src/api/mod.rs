8fbf9ffa9e948b27f145cdae5125a87496e9a3e9
code_vibe <mrsamoladapo@gmail.com>
2025-03-07 22:41:19 +0100

    Updated mod file

--- cli_app/src/api/mod.rs
+++ cli_app/src/api/mod.rs
@@ -4,6 +4,7 @@ use crate::state::ApplicationState;
 
 mod handlers;
 mod v1;
+mod response;
 
 pub fn configure(state: Arc<ApplicationState>) -> Router {
     Router::new().nest("/v1", v1::configure(state))
