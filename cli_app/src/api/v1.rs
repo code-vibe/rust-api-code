14ee309febb3eccf6e445639daeff279105b9e03
code_vibe <mrsamoladapo@gmail.com>
2025-03-07 22:40:53 +0100

    Updated v1 file

--- cli_app/src/api/v1.rs
+++ cli_app/src/api/v1.rs
@@ -1,13 +1,34 @@
 use std::sync::Arc;
 use super::handlers;
-use axum::routing::get;
+use axum::routing::{delete, get, post, put};
 use axum::Router;
+use crate::services;
 use crate::state::ApplicationState;
 
 pub fn configure(state: Arc<ApplicationState>) -> Router {
     Router::new()
         .route(
             "/hello",
-            get(handlers::hello::hello).with_state(state)
+            get(handlers::hello::hello).with_state(state.clone()),
+        )
+        .route(
+            "/posts",
+            post(handlers::posts::create).with_state(state.clone()),
+        )
+        .route(
+            "/posts",
+            get(handlers::posts::list).with_state(state.clone()),
+        )
+        .route(
+            "/posts/:id",
+            get(handlers::posts::get).with_state(state.clone()),
+        )
+        .route(
+            "/posts/:id",
+            put(handlers::posts::update).with_state(state.clone()),
+        )
+        .route(
+            "/posts/:id",
+            delete(handlers::posts::delete).with_state(state),
         )
 }
