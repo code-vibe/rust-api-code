ad80e7f7ae09ad6058ed4d3d3ad3dc3b47141f50
code_vibe <mrsamoladapo@gmail.com>
2025-03-11 05:21:19 +0100

    Updated handler post file

--- cli_app/src/api/handlers/posts.rs
+++ cli_app/src/api/handlers/posts.rs
@@ -1,47 +1,44 @@
 use std::sync::Arc;
 use axum::extract::{Path, State};
+use axum::http::StatusCode;
 use axum::Json;
-use axum_restful::AppError;
+use crate::api::errors::AppError;
 use crate::api::response::post::{ListPostsResponse, SinglePostResponse};
 use crate::model::{CreatePostRequest, UpdatePostRequest};
 use crate::services::post::PostService;
 use crate::state::ApplicationState;
 
+
 pub async fn create(
     State(state): State<Arc<ApplicationState>>,
     Json(payload): Json<CreatePostRequest>,
 ) -> Result<Json<SinglePostResponse>, AppError> {
-    let post = state.post_service.create_post(payload).await.expect("create post");
+    let post = state.post_service.create_post(payload).await?;
 
     let response = SinglePostResponse { data: post };
-
     Ok(Json(response))
 }
-
 pub async fn update(
     State(state): State<Arc<ApplicationState>>,
     Path(id): Path<i64>,
     Json(payload): Json<UpdatePostRequest>,
 ) -> Result<Json<SinglePostResponse>, AppError> {
-    let post = state.post_service.update_post( payload).await.expect("update post") ;
-
+    let post = state.post_service.update_post(payload).await?;
     let response = SinglePostResponse { data: post };
 
     Ok(Json(response))
 }
-
 pub async fn delete(
     State(state): State<Arc<ApplicationState>>,
     Path(id): Path<i64>,
 ) -> Result<Json<()>, AppError> {
-    state.post_service.delete_post(id).await.expect("Can't delete post");
-
+    state.post_service.delete_post(id).await?;
     Ok(Json(()))
 }
 pub async fn list(
     State(state): State<Arc<ApplicationState>>,
 ) -> Result<Json<ListPostsResponse>, AppError> {
-    let posts = state.post_service.get_all_posts().await.expect("Can't list posts");
+    let posts = state.post_service.get_all_posts().await?;
 
     let response = ListPostsResponse { data: posts };
 
@@ -60,8 +57,6 @@ pub async fn get(
 
             Ok(Json(response))
         }
-        Err(e) => Err(AppError::from((AppError::InternalServer {
-            location
-        }))),
+        Err(e) => Err(AppError::from((StatusCode::NOT_FOUND, e))),
     }
 }
