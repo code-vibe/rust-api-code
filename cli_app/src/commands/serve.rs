d1d40b30e228046c67a1a50f6ce1368d837fd624
code_vibe <mrsamoladapo@gmail.com>
2025-03-15 22:19:57 +0100

    Introduced logging to application

--- cli_app/src/commands/serve.rs
+++ cli_app/src/commands/serve.rs
@@ -1,19 +1,62 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::Arc;
-use axum::{Extension, Router, ServiceExt};
 use clap::{value_parser, Arg, ArgMatches, Command};
-use tracing::{subscriber, Level};
+use tracing::{Level};
 use tower_http::trace::TraceLayer;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::fmt;
-use tracing_subscriber::fmt::layer;
+use tracing_subscriber::{fmt, EnvFilter, Layer};
 use tracing_subscriber::util::SubscriberInitExt;
+use crate::model::{encrypt_password, UserStatus};
+use crate::services::user::{CreateUserRequest, UserService};
 use crate::settings::Settings;
 use crate::state::ApplicationState;
+use crate::settings::OtlpTarget;
+use opentelemetry::trace::{TraceError, TracerProvider};
+use opentelemetry::{global, KeyValue};
+use opentelemetry_otlp::{WithExportConfig, WithHttpConfig};
+use opentelemetry_sdk::trace::{RandomIdGenerator, Sampler, Tracer};
+use opentelemetry_sdk::{runtime, trace, Resource};
+use std::collections::HashMap;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 
 pub const COMMAND_NAME: &str = "serve";
 
+pub fn init_tracer(otlp_target: &OtlpTarget) -> Result<Tracer, TraceError> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    let otlp_endpoint = otlp_target.address.as_str();
+
+    let mut builder = opentelemetry_otlp::SpanExporter::builder()
+        .with_http()
+        .with_endpoint(otlp_endpoint);
+
+    if let Some(authorization) = &otlp_target.authorization {
+        let mut headers = HashMap::new();
+        headers.insert(String::from("Authorization"), authorization.clone());
+        builder = builder.with_headers(headers);
+    };
+
+    let exporter = builder.build()?;
+
+    let tracer_provider = trace::TracerProvider::builder()
+        .with_batch_exporter(exporter, runtime::Tokio)
+        .with_config(
+            trace::Config::default()
+                .with_sampler(Sampler::AlwaysOn)
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(64)
+                .with_max_attributes_per_span(16)
+                .with_max_events_per_span(16)
+                .with_resource(Resource::new(vec![KeyValue::new(
+                    "service.name",
+                    "sample_application",
+                )])),
+        )
+        .build();
+
+    Ok(tracer_provider.tracer("sample_application"))
+}
 pub fn configure() -> Command {
     Command::new(COMMAND_NAME).about("Starts HTTP Server").arg(
         Arg::new("port")
@@ -40,25 +83,43 @@ fn start_tokio(port: u16, settings: &Settings) -> anyhow::Result<()> {
         .enable_all()
         .build()?
         .block_on(async move {
+            let telemetry_layer = if let Some(otlp_target) = settings.logging.otlp_target.clone() {
+                println!("OTLP Target from Settings: {:?}", otlp_target); // Print the value from your settings
+
+                let tracer = init_tracer(&otlp_target)?;
+                Some(tracing_opentelemetry::layer().with_tracer(tracer))
+            } else {
+                None
+            };
+
+            let stdout_log = tracing_subscriber::fmt::layer().with_filter(
+                tracing_subscriber::EnvFilter::try_from_default_env()
+                    .unwrap_or(tracing_subscriber::EnvFilter::new("info")),
+            );
+
             let subscriber = tracing_subscriber::registry()
-                .with(LevelFilter::from_level(Level::TRACE))
-                .with(fmt::Layer::default());
+                .with(telemetry_layer)
+                .with(stdout_log);
 
             subscriber.init();
 
-            let state = Arc::new(ApplicationState::new(settings)?);
-            let router = crate::api::configure(state);
-                //.layer(TraceLayer::new_for_http());
+            let db_url = settings
+                .database
+                .url
+                .clone()
+                .expect("Database URL is not set");
+            let pool = sqlx::MySqlPool::connect(&db_url).await?;
 
-            let addr = SocketAddr::new(
-                IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
-                port
-            );
+            let state = Arc::new(ApplicationState::new(settings, pool)?);
+            let router = crate::api::configure(state).layer(TraceLayer::new_for_http());
+
+            let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
 
             let listener = tokio::net::TcpListener::bind(addr).await?;
             axum::serve(listener, router.into_make_service()).await?;
 
             Ok::<(), anyhow::Error>(())
         })?;
+
     Ok(())
 }
