73ccd6c6a28f4a9fcaa992e968012be7485114a5
code_vibe <mrsamoladapo@gmail.com>
2025-03-15 22:29:31 +0100

    Chnaged from Inmemory Db to mariadb

--- cli_app/src/services/user.rs
+++ cli_app/src/services/user.rs
@@ -1,6 +1,7 @@
 use crate::model::{User, UserStatus};
 use chrono::{DateTime, Utc};
 use std::collections::HashMap;
+use sqlx::MySqlPool;
 use tokio::sync::Mutex;
 
 #[allow(async_fn_in_trait)]
@@ -116,5 +117,125 @@ impl UserService for InMemoryUserService {
             }
             Some(_) => Ok(()),
         }
+    }
+}
+pub struct MySQLUserService {
+    pub pool: MySqlPool,
+}
+
+impl MySQLUserService {
+    pub fn new(pool: MySqlPool) -> Self {
+        Self { pool }
+    }
+}
+
+
+
+impl UserService for MySQLUserService {
+    async fn get_user_by_id(&self, id: i64) -> anyhow::Result<User> {
+        let res = sqlx::query!(
+            r#"
+            SELECT id, username, password, status, created, updated, last_login
+            FROM users
+            WHERE id = ?
+            "#,
+            id
+        );
+
+        res.fetch_one(&self.pool)
+            .await
+            .map(|row| User {
+                id: row.id as i64,
+                username: row.username,
+                password: row.password,
+                status: UserStatus::from(row.status),
+                created: row.created.unwrap_or_default(),
+                updated: row.updated.unwrap_or_default(),
+                last_login: row.last_login,
+            })
+            .map_err(|e| anyhow::anyhow!(e).context(format!("Failed to get user by id: {}", id)))
+    }
+
+    async fn get_user_by_name(&self, name: &str) -> anyhow::Result<User> {
+        let res = sqlx::query!(
+            r#"
+            SELECT id, username, password, status, created, updated, last_login
+            FROM users
+            WHERE username = ?
+            "#,
+            name
+        );
+
+        res.fetch_one(&self.pool)
+            .await
+            .map(|row| User {
+                id: row.id as i64,
+                username: row.username,
+                password: row.password,
+                status: UserStatus::from(row.status),
+                created: row.created.unwrap_or_default(),
+                updated: row.updated.unwrap_or_default(),
+                last_login: row.last_login,
+            })
+            .map_err(|e| {
+                anyhow::anyhow!(e).context(format!("Failed to get user by name: {}", name))
+            })
+    }
+
+    async fn create_user(&self, req: CreateUserRequest) -> anyhow::Result<User> {
+        let query = sqlx::query!(
+            r#"
+                INSERT INTO users ( username, password, status, created, updated, last_login )
+                VALUES ( ?, ?, ?, NOW(), NOW(), NULL )
+            "#,
+            req.username,
+            req.password,
+            i32::from(req.status)
+        );
+
+        let res = query.execute(&self.pool).await?.last_insert_id();
+
+        let id: i64 = res
+            .try_into()
+            .or_else(|_| anyhow::bail!("Failed to convert user id"))?;
+
+        let user = self.get_user_by_id(id).await?;
+
+        Ok(user)
+    }
+
+    async fn update_user(&self, id: i64, req: UpdateUserRequest) -> anyhow::Result<User> {
+        let query = sqlx::query!(
+            r#"
+                UPDATE users
+                SET username = ?, password = ?, status = ?, updated = NOW(), last_login = ?
+                WHERE id = ?
+            "#,
+            req.username,
+            req.password,
+            i32::from(req.status),
+            req.last_login,
+            id
+        );
+
+        query.execute(&self.pool).await?;
+
+        let user = self.get_user_by_id(id).await?;
+
+        Ok(user)
+    }
+
+    async fn delete_user(&self, id: i64) -> anyhow::Result<()> {
+        let query = sqlx::query!(
+            r#"
+                DELETE FROM users
+                WHERE id = ?
+            "#,
+            id
+        );
+
+        query.execute(&self.pool).await?;
+
+        Ok(())
     }
 }
