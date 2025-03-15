06d6b01d483759d6c35d66c2169ddf0542b14ee1
code_vibe <mrsamoladapo@gmail.com>
2025-03-15 22:25:44 +0100

    Added mysql db to the Application state

--- cli_app/src/state.rs
+++ cli_app/src/state.rs
@@ -1,20 +1,21 @@
 use std::sync::Arc;
 use arc_swap::ArcSwap;
+use sqlx::MySqlPool;
 use crate::services::post::InMemoryPostService;
-use crate::services::user::InMemoryUserService;
+use crate::services::user::{InMemoryUserService, MySQLUserService};
 use crate::settings::Settings;
 
 pub struct ApplicationState {
     pub settings: ArcSwap<Settings>,
-    pub user_service: Arc<InMemoryUserService>,
+    pub user_service: Arc<MySQLUserService>,
     pub post_service : Arc<InMemoryPostService>,
 }
 
 impl ApplicationState {
-    pub fn new(settings: &Settings) -> anyhow::Result<Self> {
+    pub fn new(settings: &Settings, pool: MySqlPool) -> anyhow::Result<Self> {
         Ok(Self {
             settings: ArcSwap::new(Arc::new((*settings).clone())),
-            user_service: Arc::new(InMemoryUserService::default()),
+            user_service: Arc::new(MySQLUserService::new(pool)),
             post_service: Arc::new(InMemoryPostService::default()),
         })
     }
